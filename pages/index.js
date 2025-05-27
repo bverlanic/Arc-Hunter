8d82ebcc3e0d771f866fda5aaa185f7c5e981261
bverlanic <bv@fdn9.com>
2025-05-27 12:59:32 -0700

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: "2rem", fontFamily: "sans-serif" }}>
+      <h1>Arc Hunter</h1>
+      <p>This is your architecture trend hunting tool.</p>
+    </div>
+  );
+}
